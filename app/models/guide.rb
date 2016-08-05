ef3f445c67d594e46ce8ebce7d55b4181db46e21
Diego Maranhão <dmaranhao@gmail.com>
2016-08-04 20:21:43 -0400

    Guide have missing avatar

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -3,7 +3,8 @@ class Guide < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100>' }
+  has_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100>' },
+                             default_url: 'missing.png'
   validates_attachment_content_type :avatar, content_type: %r{\Aimage\/.*\Z}
 
   has_many :tour
