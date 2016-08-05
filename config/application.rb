a849e2ec30dcd4502eb8dd899ad1da99dfe0e342
Leonardo Lucena <leonardo@Paulos-MacBook-Air.local>
2016-08-05 11:33:09 -0300

    inserindo pt-BR.yml e removendo mensagens login/logout do devise

--- config/application.rb
+++ config/application.rb
@@ -10,16 +10,11 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Aventura
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = :'pt-BR'
   end
 end
